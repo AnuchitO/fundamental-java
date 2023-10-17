beeda37263932536374c9255ffba3250a45a2221
Nong AnuchitO <anuchit.prasertsang@gmail.com>
2023-10-17 21:27:35 +0700

    initialize exchange rates project

---
+++ exchange/src/main/java/dev/anuchito/exchange/ExchangeApplication.java
@@ -0,0 +1,13 @@
+package dev.anuchito.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeApplication.class, args);
+	}
+
+}
---
+++ exchange/src/test/java/dev/anuchito/exchange/ExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anuchito.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
