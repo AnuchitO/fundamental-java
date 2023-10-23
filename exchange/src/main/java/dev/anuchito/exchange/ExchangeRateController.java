dc2415423cd7008f1c632bf61b0c469539b7e0ac
Nong AnuchitO <anuchito.code@gmail.com>
2023-10-23 16:44:07 +0700

    [Nong] simplify exchange response

--- exchange/src/main/java/dev/anuchito/exchange/ExchangeRateController.java
+++ exchange/src/main/java/dev/anuchito/exchange/ExchangeRateController.java
@@ -2,9 +2,7 @@ package dev.anuchito.exchange;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,11 +32,12 @@ public class ExchangeRateController {
 
 	@GetMapping("/rates")
 	public ExchangeResponse getLatestRate() {
-		List<Rate> exhangeRate = List.of(new Rate("EUR", 0.95), new Rate("GBP", 0.85), new Rate("JPY", 135.00),
-				new Rate("THB", 36.46));
+		Map<String, Double> rates = Map.of(
+				"EUR", 0.95,
+				"GBP", 0.85,
+				"JPY", 135.00,
+				"THB", 36.46);
 
-		Map<String, Double> rates = exhangeRate.stream().map(rate -> Map.entry(rate.getCurrencyCode(), rate.getRate()))
-				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 		return new ExchangeResponse("USD", rates);
 	}
 }
